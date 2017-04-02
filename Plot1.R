ca050f7cbcd5b67408f50befaf3c5da563a604c4
Noel Temena <ntemena720@gmail.com>
2017-04-02 08:40:08 -0700

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,19 @@
+library(graphics)
+df <- read.table("data.txt", header = TRUE, stringsAsFactors = FALSE, sep = ";") #read data
+date_time <- strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S") #combine data and time column
+df <- cbind(date_time,df) # add new date and time column
+df$Date <- as.Date(df$Date, "%d/%m/%Y") #convert char date to date class
+febdate <- subset(df, Date > "2007-01-31" & Date < "2007-02-03") # get only the feb 1 and 2 2003 dates
+febdate$Global_active_power <- as.numeric(febdate$Global_active_power ) #covert column to num
+febdate$Global_reactive_power <- as.numeric(febdate$Global_reactive_power ) #covert column to num
+febdate$Voltage <- as.numeric(febdate$Voltage ) #covert column to num
+febdate$Global_intensity <- as.numeric(febdate$Global_intensity ) #covert column to num
+febdate$Sub_metering_1 <- as.numeric(febdate$Sub_metering_1 ) #covert column to num
+febdate$Sub_metering_2 <- as.numeric(febdate$Sub_metering_2 ) #covert column to num
+#plot1
+png(filename="plot1.png")
+hist(febdate$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power") #Global Active Power label
+dev.off()
+
+
+
---
+++ Plot4.R
@@ -0,0 +1,27 @@
+library(graphics)
+df <- read.table("data.txt", header = TRUE, stringsAsFactors = FALSE, sep = ";") #read data
+date_time <- strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S") #combine data and time column
+df <- cbind(date_time,df) # add new date and time column
+df$Date <- as.Date(df$Date, "%d/%m/%Y") #convert char date to date class
+febdate <- subset(df, Date > "2007-01-31" & Date < "2007-02-03") # get only the feb 1 and 2 2003 dates
+febdate$Global_active_power <- as.numeric(febdate$Global_active_power ) #covert column to num
+febdate$Global_reactive_power <- as.numeric(febdate$Global_reactive_power ) #covert column to num
+febdate$Voltage <- as.numeric(febdate$Voltage ) #covert column to num
+febdate$Global_intensity <- as.numeric(febdate$Global_intensity ) #covert column to num
+febdate$Sub_metering_1 <- as.numeric(febdate$Sub_metering_1 ) #covert column to num
+febdate$Sub_metering_2 <- as.numeric(febdate$Sub_metering_2 ) #covert column to num
+#plot4
+png(filename="plot4.png")
+par(mfrow = c(2,2), oma = c(0,1,0,1))
+with(febdate, plot(date_time,Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+with(febdate, plot(date_time,Voltage, type = "l", ylab = "Voltage", xlab = "datetime"))
+plot(febdate$date_time,febdate$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
+lines(febdate$date_time,febdate$Sub_metering_2, type = "l", col = "red" )
+lines(febdate$date_time,febdate$Sub_metering_3, type = "l", col = "blue" )
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1),col = c("black", "red", "blue"))
+with(febdate, plot(date_time,Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime"))
+dev.off()
+
+
+
+
